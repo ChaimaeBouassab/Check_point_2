54f1726e0080a76acc0e255cb1c87a64715978f3
pc <shaimaabouassab@gmail.com>
2023-08-12 16:52:02 +0100

    commits

--- check_point.js
+++ check_point.js
@@ -1,35 +1,22 @@
-// Function to calculate the dot product of two vectors
-function dot_product(v1, v2) {
+function areVectorsOrthogonal(v1, v2) {
     if (v1.length !== v2.length) {
-      throw new Error("Vectors must have the same length.");
+      throw new Error("Vector dimensions do not match.");
     }
 
-    let result = 0;
+    let dotResult = 0;
     for (let i = 0; i < v1.length; i++) {
-      result += v1[i] * v2[i];
+      dotResult += v1[i] * v2[i];
     }
 
-    return result;
+    const epsilon = 1e-10;
+    return Math.abs(dotResult) < epsilon;
   }
 
-  // Function to check if two vectors are orthogonal
-  function areOrthogonal(v1, v2) {
-    const dotProduct = dot_product(v1, v2);
-    return dotProduct === 0;
-  }
-
-  // Example usage:
-  const vectors = [
-    [1, 2, 5],
-    [4, 5, 6],
-    [-1, -2, 1],
-  ];
-
-  for (let i = 0; i < vectors.length; i++) {
-    for (let j = i + 1; j < vectors.length; j++) {
-      const v1 = vectors[i];
-      const v2 = vectors[j];
-      const isOrthogonal = areOrthogonal(v1, v2);
-      console.log(`Vectors [${v1}] and [${v2}] are orthogonal: ${isOrthogonal}`);
+  function testVectorsForOrthogonality(vectorPairs) {
+    for (const [v1, v2] of vectorPairs) {
+      const isOrthogonal = areVectorsOrthogonal(v1, v2);
+      console.log(
+        `Vectors ${v1} and ${v2} are ${isOrthogonal ? "orthogonal" : "not orthogonal"}.`
+      );
     }
   }
